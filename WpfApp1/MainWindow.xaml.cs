6f27028d1272d6bc106f19dc312e771eeb0ebc81
manuel ernesto alvarez figueroa <manuel123317@hotmail.com>
2018-09-27 08:47:57 -0700

    Esta mas pro

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -27,46 +27,103 @@ namespace WpfApp1
 
         private void CalcularRectangulo_Click(object sender, RoutedEventArgs e)
         {
-            float baserectangulo = float.Parse(BaseRectangulo.Text);
+           /* float baserectangulo = float.Parse(BaseRectangulo.Text);
             float alturarectangulo = float.Parse(AlturaRectangulo.Text);
 
   
             float resultado = baserectangulo * alturarectangulo;
 
-            ResultadoRectangulo.Text = resultado.ToString() + "U²";
+            ResultadoRectangulo.Text = resultado.ToString() + "U²";*/
 
 
         }
 
         private void CalcularTriangulo_Click(object sender, RoutedEventArgs e)
         {
-            float basetriangulo = float.Parse(BaseTriangulo.Text);
+            /*float basetriangulo = float.Parse(BaseTriangulo.Text);
             float alturatriangulo = float.Parse(AlturaTriangulo.Text);
 
             float resultado = (basetriangulo * alturatriangulo) / 2;
 
-            ResultadoTriangulo.Text = resultado.ToString() + "U²";
+            ResultadoTriangulo.Text = resultado.ToString() + "U²";*/
         }
 
         private void CalcularCirculo_Click(object sender, RoutedEventArgs e)
         {
-            float radiocirculo = float.Parse(RadioCirculo.Text);
+           /* float radiocirculo = float.Parse(RadioCirculo.Text);
             float pi = Convert.ToSingle(Math.PI);
 
             float resultado = pi*(radiocirculo * radiocirculo);
 
-            ResultadoCirculo.Text = resultado.ToString() + "U²";
+            ResultadoCirculo.Text = resultado.ToString() + "U²";*/
         }
 
         private void CalcularTrapecio_Click(object sender, RoutedEventArgs e)
         {
-            float basemayor = float.Parse(BaseMayorTrapecio.Text);
+           /* float basemayor = float.Parse(BaseMayorTrapecio.Text);
             float basemenor = float.Parse(BaseMenorTrapecio.Text);
             float altura = float.Parse(AlturaTrapecio.Text);
 
             float resultado = (basemayor * basemenor * altura) / 2;
 
-            ResultadoTrapecio.Text = resultado.ToString() + "U²";
+            ResultadoTrapecio.Text = resultado.ToString() + "U²"; */
+        }
+
+        private void cbTipoFigura_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            panelConfigacion.Children.Clear();
+
+            switch (cbTipoFigura.SelectedIndex)
+            {
+                case 0: //Rectangulo
+                    panelConfigacion.Children.Add(new ControlAreaRectangulo());
+                    break;
+
+                case 1: //Triangulo
+                    panelConfigacion.Children.Add(new ControlAreaTriangulo());
+                    break;
+
+                case 2: //Circulo
+                    panelConfigacion.Children.Add(new ControlAreaCirculo());
+                    break;
+
+                case 3:  //Trapecio
+                    panelConfigacion.Children.Add(new ControlAreaTrapecio());
+                    break;
+
+                default:
+
+                    break;
+            }
+        }
+
+        private void Calcular_Click(object sender, RoutedEventArgs e)
+        {
+            double area = 0.0;
+            switch (cbTipoFigura.SelectedIndex)
+            {
+                case 0: //Rectangulo
+               
+                    break;
+
+                case 1: //Triangulo
+                  
+                    break;
+
+                case 2: //Circulo
+                   
+                    break;
+
+                case 3:  //Trapecio
+                 
+                    break;
+
+                default:
+
+                    break;
+            }
+            Resultado.Text = area.ToString();
         }
     }
 }
